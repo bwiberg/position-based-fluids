8ee1a787739354997ad22b41320da2647510d46b
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-10-20 19:31:48 -0700

    Add RESPATH macro to find files in /res folder

--- src/util/paths.hpp
+++ src/util/paths.hpp
@@ -4,3 +4,4 @@
 
 #define SHADERPATH(filename) std::string(SHADERS_FOLDER) + std::string(filename)
 #define KERNELPATH(filename) std::string(KERNELS_FOLDER) + std::string(filename)
+#define RESPATH(filename) std::string(RES_FOLDER) + std::string(filename)
