3034c9bcdf41cbd313a7765de5fab36f195d3502
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-10-27 00:57:23 -0700

    Extract inline function definitions from Fluid.hpp to Fluid.cpp

--- src/simulation/Fluid.cpp
+++ src/simulation/Fluid.cpp
@@ -1,4 +1,18 @@
-//
-// Created by Benjamin Wiberg on 10/27/16.
-//
+#include "Fluid.hpp"
 
+namespace pbf {
+    std::unique_ptr<Fluid> Fluid::GetDefault() {
+        std::unique_ptr<Fluid> fluid = util::make_unique<Fluid>();
+
+        fluid->kernelRadius  = 0.1f;
+        fluid->restDensity   = 6378.0f;
+        fluid->deltaTime     = 0.0083f;
+        fluid->epsilon       = 600.0f;
+        fluid->s_corr        = 0.0001f;
+        fluid->delta_q       = 0.03f;
+        fluid->n             = 4;
+        fluid->c             = 0.01f;
+
+        return fluid;
+    }
+}
--- src/simulation/Fluid.hpp
+++ src/simulation/Fluid.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <CL/cl.hpp>
 #include "util/make_unique.hpp"
 
 namespace pbf {
@@ -26,19 +27,4 @@ namespace pbf {
         // Artificial Viscosity (c): <= 0.01"
         cl_float c;
     };
-
-    inline std::unique_ptr<Fluid> Fluid::GetDefault() {
-        std::unique_ptr<Fluid> fluid = util::make_unique<Fluid>();
-
-        fluid->kernelRadius  = 0.1f;
-        fluid->restDensity   = 6378.0f;
-        fluid->deltaTime     = 0.0083f;
-        fluid->epsilon       = 600.0f;
-        fluid->s_corr        = 0.0001f;
-        fluid->delta_q       = 0.03f;
-        fluid->n             = 4;
-        fluid->c             = 0.01f;
-
-        return fluid;
-    }
 }
