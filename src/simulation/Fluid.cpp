7111b23b00c9821ac1e91331192a0480e2791cdc
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-11-04 16:47:19 -0700

    Set nice fluid parameters as default

--- src/simulation/Fluid.cpp
+++ src/simulation/Fluid.cpp
@@ -7,18 +7,18 @@ namespace pbf {
     std::unique_ptr<Fluid> Fluid::GetDefault() {
         std::unique_ptr<Fluid> fluid = util::make_unique<Fluid>();
 
-        fluid->kernelRadius = 0.1f;
-        fluid->numSubSteps = 1;
-        fluid->restDensity  = 6378.0f;
+        fluid->kernelRadius = 0.11f;
+        fluid->numSubSteps = 4;
+        fluid->restDensity  = 8000.0f;
         fluid->deltaTime    = 0.0083f;
-        fluid->epsilon      = 600.0f;
-        fluid->k            = 0.0001f;
+        fluid->epsilon      = 1000.0f;
+        fluid->k            = 0.0005f;
         fluid->delta_q      = 0.03f;
         fluid->n            = 4;
-        fluid->c            = 0.01f;
-        fluid->k_vc         = 1.0f;
+        fluid->c            = 0.05f;
+        fluid->k_vc         = 25.0f;
 
-        fluid->kBoundsDensity = 1.0f;
+        fluid->kBoundsDensity = 50.0f;
 
         return fluid;
     }
